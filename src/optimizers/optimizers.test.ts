c02fcc1e05618d7d0badc3a1c405f893d82f3ba0
Gonzalo Uceda <gonzalo.uceda@devo.com>
2022-08-17 20:45:48 +0200

    test: add suits to optimizers

--- src/optimizers/optimizers.test.ts
+++ src/optimizers/optimizers.test.ts
@@ -1,14 +1,45 @@
+import { fitts as fittsObjs } from "../objectiveFunctions";
 import { random as randomOptimizer } from "./random";
 
 describe("optimizers", () => {
-  test("ramdom", () => {
-    expect(
-      randomOptimizer({
-        iterations: 10,
-        seed: ["c", "a", "b"],
-        params: {},
-        objectiveFunction: ({ candidate }) => candidate.indexOf("a"),
-      }).bestDesign[0]
-    ).toBe("a");
+  describe("ramdom", () => {
+    test("mock objective", () => {
+      expect(
+        randomOptimizer({
+          iterations: 10,
+          seed: ["c", "a", "b"],
+          params: {},
+          objectiveFunction: ({ candidate }) => candidate.indexOf("a"),
+        }).bestDesign[0]
+      ).toBe("a");
+    });
+    test("fitts objective", () => {
+      const iterations = 100000;
+      const seed = ["open", "save", "close", "undo", "saveas", "print", "redo"];
+      const frequencies = {
+        open: 8,
+        save: 10,
+        close: 4,
+        undo: 7,
+        saveas: 9,
+        print: 6,
+        redo: 5,
+      };
+      const { bestDesign } = randomOptimizer({
+        iterations,
+        seed,
+        params: { frequencies },
+        objectiveFunction: fittsObjs,
+      });
+      expect(bestDesign).toStrictEqual([
+        "save",
+        "saveas",
+        "open",
+        "undo",
+        "print",
+        "redo",
+        "close",
+      ]);
+    });
   });
 });
