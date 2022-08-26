7fc2f9e37db638744c15863d3115415700a2acf6
Gonzalo Uceda <gonzalo.uceda@devo.com>
2022-08-26 09:33:29 +0200

    fix: fix import paths

--- demo/optimizeCombined.ts
+++ demo/optimizeCombined.ts
@@ -3,30 +3,48 @@ import {
   IAssociations,
   IFrequencies,
   MenuType,
-} from '../declarations/dataStructures';
-import { InnerObjectiveFnType } from '../objectiveFunctions';
+} from '../src/declarations/dataStructures';
+import { InnerObjectiveFnType } from '../src/objectiveFunctions';
 
 // Group separators have to be provided as dashes ('-').
-const menuEntries = ['open', 'save', '-', '-', 'close', 'saveas'];
+const menuEntries = [
+  'open',
+  'save',
+  'close',
+  'saveas',
+  'print',
+  'undo',
+  'redo',
+  'about',
+  '-',
+  '-',
+  '-',
+];
 
-const frequencies: IFrequencies = { open: 4, save: 10, close: 1, saveas: 2 };
+const frequencies: IFrequencies = {
+  open: 4,
+  save: 10,
+  close: 1,
+  saveas: 2,
+  undo: 34,
+  redo: 17,
+};
 
 // Associations with null size are not necessary.
 const associations: IAssociations = {
   open: { close: 1 },
   save: { saveas: 0.9 },
   close: { save: 0.4 },
+  undo: { redo: 1 },
+  about: { print: 0.5 },
 };
 
 const combinedObjFn: InnerObjectiveFnType = (candidate: MenuType) => {
-  return (
-    objectives.fitts({ frequencies })(candidate) +
-    0.5 * objectives.associations({ associations })(candidate)
-  );
+  return objectives.associations({ associations })(candidate);
 };
 
 const { bestDesign, bestScore } = optimizers.random({
-  iterations: 1000,
+  iterations: 100000,
   seed: menuEntries,
   objectiveFunction: combinedObjFn,
 });
