9d27e4b6a9cdfb4a39c26c0dc038b947ac684486
Gonzalo Uceda Castro <gon_u@hotmail.com>
2022-10-13 20:45:38 +0200

    test: fix types

--- src/optimizers/declarations.ts
+++ src/optimizers/declarations.ts
@@ -4,7 +4,8 @@ import { InnerObjectiveFnType } from '../objectiveFunctions';
 export interface IOptimizerParams {
   seed: MenuType;
   objectiveFunction: InnerObjectiveFnType;
-  [key: string]: unknown;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
 }
 
 export interface IOptimizerResult {
