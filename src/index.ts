e9defc6f748aa6bffec13a607f15e639726bf8e3
Gonzalo Uceda <gonzalo.uceda@devo.com>
2023-02-20 18:03:02 +0100

    fix: export all components

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
 // LAWS
-import { fitts as fittsLaw } from './laws/fitts';
+import * as laws from './laws';
 // OBJECTIVE FUNCTIONS
-import { associations as objectiveAssoc } from './objectiveFunctions/associations';
-import { fitts as objectiveFitts } from './objectiveFunctions/fittsLaw';
+import * as objectives from './objectiveFunctions';
 // OPTIMIZERS
-import { random as randomOptimizer } from './optimizers';
-import { simulatedAnnealing as simulatedAnnealingOptimizer } from './optimizers';
-// SAMPLERS
-// import { random as randomSampler } from "./samplers/random";
+import * as optimizers from './optimizers';
 
-const laws = { fitts: fittsLaw };
-const objectives = { associations: objectiveAssoc, fitts: objectiveFitts };
-const optimizers = {
-  random: randomOptimizer,
-  simulatedAnnealing: simulatedAnnealingOptimizer,
-};
-// const samplers = { random: randomSampler }; // INTERNAL
-
-export { laws, objectives, optimizers /* samplers */ };
+export { laws, objectives, optimizers };
